333524879697e3c5b741fa4e5f2993467f85f5cb
virtualknight8 <ankitkhola2@gmail.com>
2023-06-29 21:37:27 +0530

    add CRUD functionality

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
- 
+
+# Configure the database connection parameters
 db_params = {
     'host': 'localhost',
     'database': 'inventory_management',
@@ -13,34 +14,174 @@ db_params = {
 }
 
 @app.route('/inventory', methods=['GET'])
-def get_categories():
+def get_inventory():
     # Establish a connection to the database
     conn = psycopg2.connect(**db_params)
 
     # Create a cursor object to interact with the database
     cur = conn.cursor()
 
-    # Execute the SQL query to retrieve data from the 'categories' table
-    cur.execute("SELECT * FROM categories")
+    # Execute the SQL query to retrieve inventory items with category and supplier information
+    cur.execute("""
+        SELECT p.id, p.name, p.price, p.quantity, c.name AS category, s.name AS supplier
+        FROM products p
+        JOIN categories c ON p.category_id = c.id
+        JOIN suppliers s ON p.supplier_id = s.id
+    """)
 
     # Fetch all rows of the result
-    category_rows = cur.fetchall()
+    inventory_items = cur.fetchall()
 
     # Close the cursor and the connection
     cur.close()
     conn.close()
 
     # Convert the data to a suitable format (e.g., list of dictionaries)
-    categories = []
-    for row in category_rows:
-        category = {
-            'id': row[0],
-            'name': row[1]
+    inventory_data = []
+    for item in inventory_items:
+        item_data = {
+            'id': item[0],
+            'name': item[1],
+            'price': float(item[2]),
+            'quantity': item[3],
+            'category': item[4],
+            'supplier': item[5]
         }
-        categories.append(category)
+        inventory_data.append(item_data)
 
-    # Return the categories as JSON response
-    return jsonify(categories)
+    # Return the inventory data as JSON response
+    return jsonify(inventory_data)
+    
+
+# Endpoint for creating a new inventory item
+@app.route('/inventory', methods=['POST'])
+def example_endpoint():
+    # Get the data from the request body
+        data = request.get_json()
+
+        # Extract the necessary fields from the data
+        name = data.get('name')
+        price = data.get('price')
+        quantity = data.get('quantity')
+        category_id = data.get('category_id')
+        supplier_id = data.get('supplier_id')
+
+        # Perform any necessary validation on the input data
+
+        # Establish a connection to the database
+        conn = psycopg2.connect(**db_params)
+
+        # Create a cursor object to interact with the database
+        cur = conn.cursor()
+
+        try:
+            # Execute the SQL query to insert the new inventory item
+            cur.execute("""
+                INSERT INTO products (name, price, quantity, category_id, supplier_id)
+                VALUES (%s, %s, %s, %s, %s)
+            """, (name, price, quantity, category_id, supplier_id))
+
+            # Commit the transaction
+            conn.commit()
+
+            # Return a success message or the ID of the newly created item
+            return jsonify({'message': 'Item created successfully'})
+
+        except Exception as e:
+            # Rollback the transaction in case of any errors
+            conn.rollback()
+            # Return an error message
+            return jsonify({'error': str(e)}), 500
+
+        finally:
+            # Close the cursor and the connection
+            cur.close()
+            conn.close()
+
+# Update an inventory item
+@app.route('/inventory/<item_id>', methods=['PUT'])
+def update_inventory_item(item_id):
+    # Get the updated item data from the request body
+    updated_data = request.json
+
+    # Establish a connection to the database
+    conn = psycopg2.connect(**db_params)
+    cur = conn.cursor()
+
+    try:
+        # Check if the item exists in the database
+        cur.execute("SELECT * FROM products WHERE id = %s", (item_id,))
+        existing_item = cur.fetchone()
+
+        if existing_item:
+            # Perform the update operation in the database
+            cur.execute("""
+                UPDATE products
+                SET name = %s, price = %s, quantity = %s
+                WHERE id = %s
+            """, (
+                updated_data.get('name', existing_item[1]),
+                updated_data.get('price', existing_item[2]),
+                updated_data.get('quantity', existing_item[3]),
+                item_id
+            ))
+
+            # Commit the transaction
+            conn.commit()
+
+            # Return a response indicating success
+            return jsonify({'message': 'Inventory item updated successfully'}), 200
+        else:
+            # Return a response indicating failure (item not found)
+            return jsonify({'error': 'Inventory item not found'}), 404
+    except Exception as e:
+        # Handle any errors that occur during the update operation
+        conn.rollback()
+        return jsonify({'error': 'Error updating inventory item', 'message': str(e)}), 500
+    finally:
+        # Close the cursor and the connection
+        cur.close()
+        conn.close()
+
+
+# Endpoint for deleting an inventory item
+@app.route('/inventory/<item_id>', methods=['DELETE'])
+def delete_inventory_item(item_id):
+    conn = psycopg2.connect(**db_params)
+    cur = conn.cursor()
+
+    # Delete the inventory item from the database based on the item_id
+    try:
+        # Grab the item from the item_id
+        cur.execute("SELECT * FROM products WHERE id=%s", (item_id,))
+        existing_item = cur.fetchone
+
+        # Check if the item exists in the database
+        if existing_item:
+            cur.execute("DELETE FROM products WHERE id=%s", (item_id,))
+            conn.commit()
+
+            # Return a response indicating success
+            return jsonify({'message': 'Item delted successfully'}), 200
+
+
+        else:
+            # Return a response indicating failure
+            return jsonify({'message': 'Inventory item not found'}), 404
+
+    except Exception as e:
+        # Handle any errors that may occur during execution
+        conn.rollback()
+        return jsonify({'error': 'Error deleting inventory item', 'message': str(e)}), 500
+
+    finally:
+        # Close the curson and the connection
+        cur.close()
+        conn.close()    
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- script.js
+++ script.js
@@ -1,35 +1,195 @@
+// Global variable to store the inventory data
+let inventoryData = [];
+
 // Fetch inventory data from the backend API
 function fetchInventoryData() {
-    fetch('http://localhost:5000/inventory')
+  fetch('http://localhost:5000/inventory')
     .then(response => response.json())
     .then(data => {
-      // Process the retrieved categories data
-      // Update the UI with the categories data
-      displayCategoriesData(data);
+
+       // Store the inventory data
+       inventoryData = data;
+      // Process the retrieved inventory data
+      // Update the UI with the inventory data
+      inventoryData.sort((a,b)=>a.id - b.id); // Sort the inventory list
+      displayInventoryData(data);
     })
     .catch(error => {
       // Handle any errors that occur during the request
-      console.error('Error fetching categories data:', error);
+      console.error('Error fetching inventory data:', error);
     });
 }
 
-// Display the categories data in the UI
-function displayCategoriesData(categoriesData) {
-  // Get the inventory container element
-  const inventoryContainer = document.getElementById('inventory-container');
+// Display the inventory data in the UI
+function displayInventoryData(inventoryData) {
+  // Get the inventory table element
+  const inventoryTable = document.getElementById('inventory-table');
 
-  // Clear any existing content from the inventory container
-  inventoryContainer.innerHTML = '';
+  // Clear any existing content from the inventory table
+  inventoryTable.innerHTML = '';
 
-  // Iterate over the categories data and create HTML elements to display the categories
-  categoriesData.forEach(category => {
-    const categoryElement = document.createElement('div');
-    categoryElement.classList.add('category');
-    categoryElement.textContent = category.name;
-    inventoryContainer.appendChild(categoryElement);
+  // Create the table header row
+  const headerRow = document.createElement('tr');
+  headerRow.innerHTML = `
+    <th>ID</th>
+    <th>Name</th>
+    <th>Price</th>
+    <th>Quantity</th>
+    <th>Category</th>
+    <th>Supplier</th>
+  `;
+  inventoryTable.appendChild(headerRow);
+
+  // Iterate over the inventory data and create table rows to display the items
+  inventoryData.forEach(item => {
+    const itemRow = document.createElement('tr');
+    itemRow.innerHTML = `
+      <td>${item.id}</td>
+      <td>${item.name}</td>
+      <td>${item.price}</td>
+      <td>${item.quantity}</td>
+      <td>${item.category}</td>
+      <td>${item.supplier}</td>
+      <td><button class="deletebuttons" data-id="${item.id}">Delete</button></td>
+    `;
+    inventoryTable.appendChild(itemRow);
   });
+
+  document.querySelectorAll('.deletebuttons').forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+      const buttonId = e.target.getAttribute('data-id');
+      deleteInventoryData(buttonId);
+    })
+  })
 }
 
-// Call the fetchCategoriesData function to retrieve and display the categories data
 
- 
+// Event listeners for search and filter inputs
+const searchInput = document.getElementById('search-input');
+const filterInput = document.getElementById('filter-input');
+
+searchInput.addEventListener('input', () => {
+  const query = searchInput.value.trim();
+  searchInventory(query);
+});
+
+filterInput.addEventListener('change', () => {
+  const category = filterInput.value;
+  filterInventory(category);
+});
+
+// Search inventory items by name or ID
+function searchInventory(query) {
+  const searchResults = inventoryData.filter(item => {
+    return (
+      item.name.toLowerCase().includes(query.toLowerCase()) ||
+      item.id.toString().includes(query)
+    );
+  });
+
+  displayInventoryData(searchResults);
+}
+
+// Filter inventory items by category
+function filterInventory(category) {
+  if(category == 'All'){
+    displayInventoryData(inventoryData);
+  }
+  else{
+    const filteredResults = inventoryData.filter(item => {
+      return item.category.toLowerCase() === category.toLowerCase();
+    });
+    displayInventoryData(filteredResults);
+  
+  }
+  
+}
+
+// Call the fetchInventoryData function to retrieve and display the inventory data
+fetchInventoryData();
+
+
+
+// Add event listener to the create item form
+document.getElementById('create-item-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Gather the form data
+  const formData = new FormData(this);
+
+  // Make a POST request to the backend API
+  fetch('http://localhost:5000/inventory', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(Object.fromEntries(formData))
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response and update the UI accordingly
+    console.log(data);
+    fetchInventoryData();
+  })
+  .catch(error => {
+    // Handle any errors that occur during the request
+    console.error('Error creating inventory item:', error);
+  });
+  
+});
+
+
+// Event listener for the edit form submission
+document.getElementById('update-item-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Gather the form data
+  const formData = new FormData(this);
+  const itemId = formData.get('id');
+  console.table(Object.fromEntries(formData));
+  console.log(itemId);
+
+  // Make a PUT request to the backend API to update the item
+  fetch(`http://localhost:5000/inventory/${itemId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(Object.fromEntries(formData))
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response and update the UI accordingly
+    console.log(data);
+    
+    fetchInventoryData();
+    
+  })
+  .catch(error => {
+    // Handle any errors that occur during the request
+    console.error('Error updating inventory item:', error);
+  });
+});
+
+// Funtion to delete an Inventory Data
+function deleteInventoryData(theItemId){
+  fetch(`http://localhost:5000/inventory/${theItemId}`, {
+    method:'DELETE'
+  })
+  .then(response=>{
+    if (response.ok){
+      // Item delted successfully
+      console.log('Inventory Item delted successfully');
+      //Refresh the Inventory data
+      fetchInventoryData();
+    }
+    else{
+      // Error deleting Inventory Item
+      console.log('Error deleting Inventory item');
+    }
+  })
+  .catch(error=>{
+    // Handle any errors that occur during deleting the item
+    console.log('Error deleting inventory item:', error);
+  });
+}
